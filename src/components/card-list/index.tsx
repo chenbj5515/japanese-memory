b87ac30a535000b85ce79c416a897946abf88c52
Baijin Chen <chenbj5515@163.com>
2024-02-22 00:55:13 +0900

    fix: can not upload;

--- src/components/card-list/index.tsx
+++ src/components/card-list/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { gql, useQuery, useMutation } from "@apollo/client";
+import { gql, useQuery } from "@apollo/client";
 import { useSelector, useDispatch } from "react-redux";
 import { updateHistoryLists } from "../card-list/history-lists-slice";
 import { useCookies } from "react-cookie";
-import { findShouldReviewDatas, callChatApi } from "@/utils";
+import { findShouldReviewDatas } from "@/utils";
 import { CardInHistory, CardInHome } from "../card";
 
 interface ICard {
@@ -36,26 +36,6 @@ const GET_CARD = gql`
   }
 `;
 
-const UPDATE_TRANSLATION_AND_KANA = gql`
-  mutation UpdateMemoCard(
-    $id: uuid!
-    $translation: String
-    $kana_pronunciation: String
-  ) {
-    update_memo_card_by_pk(
-      pk_columns: { id: $id }
-      _set: {
-        translation: $translation
-        kana_pronunciation: $kana_pronunciation
-      }
-    ) {
-      id
-      translation
-      kana_pronunciation
-    }
-  }
-`;
-
 interface ICard {
   content: string;
   record_file_path: string;
@@ -83,69 +63,6 @@ export function CardList(props: IProps) {
       user_id: cookies.user_id,
     },
   });
-
-  const [update] = useMutation(UPDATE_TRANSLATION_AND_KANA);
-
-  function promisefy(original_text: string, prompt: string) {
-    let tanslation = "";
-    return new Promise((resolve, reject) => {
-      console.log("call=====");
-      callChatApi(original_text, {
-        onmessage(event: any) {
-          if (event.data === "[DONE]") {
-            return;
-          }
-          const parsedData = JSON.parse(event.data);
-          const curText =
-            parsedData.choices
-              ?.map((choice: any) => {
-                if (choice.delta) {
-                  return choice.delta.content;
-                }
-                return "";
-              })
-              ?.join("") || "";
-          tanslation += curText;
-        },
-        async onclose() {
-          resolve(tanslation);
-        },
-        onerror(err: any) {
-          console.log(err, "err====");
-        },
-        prompt,
-      });
-    });
-  }
-
-  // async function handleClick() {
-  //   if (data) {
-  //     for (let { id, original_text } of data.memo_card) {
-  //       try {
-  //         const p1 = promisefy(original_text, "对我给出的日文，给出中文翻译");
-  //         const p2 = promisefy(
-  //           original_text,
-  //           "对我给出的日文，给出假名形式的读音标记"
-  //         );
-  //         const res = await Promise.all([p1, p2]);
-  //         await update({
-  //           variables: {
-  //             id,
-  //             translation: res[0],
-  //             kana_pronunciation: res[1],
-  //           },
-  //         });
-  //         console.log(res, "res======");
-  //         // 在这里处理 res
-  //       } catch (error) {
-  //         console.error("处理 memo card 时出错：", error);
-  //         // 根据需要处理错误
-  //       }
-  //     }
-  //   }
-  // }
-
-  // console.log(data, "data====");
 
   const { historyLists } = useSelector((state: any) => state.historyListsSlice);
 
@@ -206,8 +123,8 @@ export function CardList(props: IProps) {
 
   return (
     <>
-      {localCards?.map(({ original_text, id }: any) => (
-        <CardInHome key={id} originalText={original_text} />
+      {localCards?.map(({ originalText, id }: any) => (
+        <CardInHome key={id} originalText={originalText} />
       ))}
     </>
   );
